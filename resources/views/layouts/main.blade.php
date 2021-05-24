2842c7f19a0bfe2f69c216bdc8b897c65fe258d7
VillarrealAlex <59488539+VillarrealAlex@users.noreply.github.com>
2021-05-24 15:07:49 -0500

    URL

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,7 @@
 <body>
     <header class="site-header sticky-top py-1">
         <nav class="container d-flex flex-column flex-md-row justify-content-between">
-          <a class="py-2" href="#" aria-label="Product">
+          <a class="py-2" href="{{url('/')}}" aria-label="Product">
            <img src="{{asset('/images/cat.png')}}" alt="" width="37px" height="37px">
           </a>
           
